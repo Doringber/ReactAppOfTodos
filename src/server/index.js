ac423fa0a690847495c8cddd63e84f3e302229ef
doringber123 <dori@pango.co.il>
2021-10-01 23:12:52 +0300

    dfs

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const PORT = process.env.PORT || 3001;
+// const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -9,7 +9,19 @@ app.get("/api", (req, res) => {
   });
 
   
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server listening on ${PORT}`);
+// });
 
+server.listen(3000, '0.0.0.0');
+
+
+// module.exports = ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 443),
+//   admin: {
+//     auth: {
+//       secret: env('ADMIN_JWT_SECRET'),
+//     },
+//   },
+// });
